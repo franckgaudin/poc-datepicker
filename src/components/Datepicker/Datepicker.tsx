f6adbd90e3c7f4cc57fe61c52e175104d6632bfa
Franck Gaudin <app@franckgaudin.com>
2022-09-20 18:12:22 +0000

    clean code

--- src/components/Datepicker/Datepicker.tsx
+++ src/components/Datepicker/Datepicker.tsx
@@ -1,19 +1,15 @@
 import * as React from 'react';
-import { parseDate, toCalendarDateTime, getLocalTimeZone, now,  CalendarDate, Time, parseDateTime, parseAbsoluteToLocal, CalendarDateTime } from "@internationalized/date";
+import { getLocalTimeZone, now, parseAbsoluteToLocal, CalendarDateTime } from "@internationalized/date";
 
 import Calendar from "../Calendar/Calendar";
 
 interface DatepickerProps {
-  date: any;
-  onChange?: (event: any) => void;
+  date: string;
+  onChange?: (event: string) => void;
 }
 
 const Datepicker = (props: DatepickerProps) => {
   const { date, onChange } = props;
-
-  // console.log('date', date)
-  // console.log(parseDateTime('2022-09-19T13:41:21.966'))
-  // console.log(parseAbsoluteToLocal('2022-09-19T13:46:19.416-04:00'))
 
   const formattedDate = parseAbsoluteToLocal(date);
   let [value, setValue] = React.useState(formattedDate);
